07e843d6b2c17b782b05dd8f2e60630c8fd69585
Roland Stens <roland.stens@gmail.com>
2020-10-07 16:54:45 -0700

    certificates and stuff

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/profile.mobileprovision ./.github/secrets/profile.mobileprovision.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_PROFILE_KEY" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
+base64 --decode <<< "$CLIENT_CERTIFICATE" > ./.github/secrets/Certificates.p12
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
-cp ./.github/secrets/profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/86ce4d81-fd7e-46b2-ae6a-3092b4af6cd7.mobileprovision
+cp ./.github/secrets/profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/4832f70b-a8ed-48fe-96e8-9314a145fdd5.mobileprovision
 
 
 security create-keychain -p "" build.keychain
